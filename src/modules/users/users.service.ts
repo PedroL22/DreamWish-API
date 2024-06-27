f3e5a651b5c7433e5712d9f8b1be9af7adcb1e0a
Pedro Lucena <pedrocontact22@gmail.com>
2024-06-27 14:03:00 -0300

    ✨ feat: Add createUser service

--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, NotFoundException } from '@nestjs/common'
+import { BadRequestException, ConflictException, Injectable, NotFoundException } from '@nestjs/common'
 import { eq } from 'drizzle-orm'
 import { db } from '~/db/connection'
 import { users } from '~/db/schema/users'
@@ -42,5 +42,30 @@ export class UsersService {
     }
 
     await db.delete(users).where(eq(users.id, id))
+  }
+
+  async createUser(data: Partial<UserModel>): Promise<Partial<UserModel> | ConflictException | BadRequestException> {
+    const checkIfUserExists = await db.select().from(users).where(eq(users.email, data.email))
+
+    if (checkIfUserExists.length) {
+      return new ConflictException('User already exists.')
+    }
+
+    if (!data.username) {
+      return new BadRequestException('Username is required.')
+    }
+
+    const user: UserModel = {
+      id: crypto.randomUUID(),
+      username: data.username,
+      email: data.email,
+      phone: data.phone,
+      firstName: data.firstName,
+      lastName: data.lastName,
+      createdAt: new Date(),
+      updatedAt: null,
+    }
+
+    await db.insert(users).values(user)
   }
 }
