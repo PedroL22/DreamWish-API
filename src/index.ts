1e686b102ce4a22d1cdc86dbfc16133f5bb1005a
Pedro Lucena <pedrocontact22@gmail.com>
2025-02-22 13:29:55 -0300

    ✨ feat: Add login endpoint with Zod validation and JWT token generation

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,40 @@
-import { Hono } from "hono";
-import { logger } from "hono/logger";
+import { zValidator } from '@hono/zod-validator'
+import { Hono } from 'hono'
+import { setCookie } from 'hono/cookie'
+import { HTTPException } from 'hono/http-exception'
+import { sign } from 'hono/jwt'
+import { logger } from 'hono/logger'
+import { z } from 'zod'
 
-const app = new Hono();
+const app = new Hono()
 
-app.get("/hello", (c) => {
-	return c.json({ hello: "Hello World!" });
-});
+const schema = z.object({
+  email: z.string().email(),
+  password: z.string(),
+})
+
+app.use(logger())
+
+app.get('/login', zValidator('json', schema), async (c) => {
+  const { email, password } = await c.req.json()
+
+  if (password !== 'testing') {
+    throw new HTTPException(401, { message: 'Invalid credentials.' })
+  }
+
+  const payload = {
+    email,
+    exp: Math.floor(Date.now() / 1000) + 60 * 60, // 1 hour
+  }
+
+  const token = await sign(payload, Bun.env.JWT_SECRET || '')
+
+  setCookie(c, 'token', token)
+
+  return c.json({ payload, token })
+})
 
 Bun.serve({
-	fetch: app.fetch,
-	port: process.env.PORT ?? 3030,
-});
+  fetch: app.fetch,
+  port: Bun.env.PORT || 3030,
+})
