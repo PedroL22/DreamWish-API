8aa10a227baf119f576bab2d98ad0c9bb5453e87
Pedro Lucena <pedrocontact22@gmail.com>
2025-02-22 12:57:14 -0300

    ✨ feat: Implement Bun server with Hello World response and add VSCode settings

--- index.ts
+++ index.ts
@@ -1,1 +1,8 @@
-console.log("Hello via Bun!");
+console.log('Hello, World!')
+
+Bun.serve({
+  fetch: (request) => {
+    return new Response('Hello, World!')
+  },
+  port: process.env.PORT ?? 3030,
+})
